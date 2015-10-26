a50012c6fc616fef7f572193193c2092d4fe853e
andrewe <andrewevansc@gmail.com>
2015-10-25 18:19:10 -0700

    Add Laravel framework.

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+class HomeController extends BaseController {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| You may wish to use controllers instead of, or in addition to, Closure
+	| based routes. That's great! Here is an example controller method to
+	| get you started. To route to this controller, just add the route:
+	|
+	|	Route::get('/', 'HomeController@showWelcome');
+	|
+	*/
+
+    /**
+     * The layout that should be used for responses.
+     */
+    protected $layout = 'layouts.master';
+
+    public function index()
+    {
+        return View::make('home.index');
+    }
+
+	public function showWelcome()
+	{
+		return View::make('hello');
+	}
+
+}
