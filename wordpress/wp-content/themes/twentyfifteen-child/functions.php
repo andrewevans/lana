1ad5787a005cce6535a7268a6a4c1c6cf7635ad2
andrewe <andrewevansc@gmail.com>
2015-11-28 20:02:11 -0800

    WIP. HTML spans for city, country in WP Post content.
    Needs jQuery selector on backend to create the MadLibs.

--- wordpress/wp-content/themes/twentyfifteen-child/functions.php
+++ wordpress/wp-content/themes/twentyfifteen-child/functions.php
@@ -18,3 +18,69 @@ function my_general_settings_fields_html()
     $value = get_option( 'site_keywords', '' );
     echo '<input type="text" id="site_keywords" name="site_keywords" value="' . $value . '" />';
 }
+
+// Callback function to insert 'styleselect' into the $buttons array
+function my_mce_buttons_2( $buttons ) {
+    array_unshift( $buttons, 'styleselect' );
+    return $buttons;
+}
+
+// Register our callback to the appropriate filter
+add_filter('mce_buttons_2', 'my_mce_buttons_2');
+
+// Callback function to filter the MCE settings
+function my_mce_before_init_insert_formats( $init_array ) {
+    // Define the style_formats array
+    $style_formats = array(
+        // Each array child is a format with it's own settings
+        array(
+            'title' => 'ML: City, Country [mine]',
+            'inline' => 'span',
+            'classes' => 'ml-city-country-mine',
+            'wrapper' => false,
+        ),
+        array(
+            'title' => 'ML: Country [mine]',
+            'inline' => 'span',
+            'classes' => 'ml-country-mine',
+            'wrapper' => false,
+        ),
+        array(
+            'title' => 'ML: City [mine]',
+            'inline' => 'span',
+            'classes' => 'ml-city-mine',
+            'wrapper' => false,
+        ),
+        array(
+            'title' => 'ML: City, Country [theirs]',
+            'inline' => 'span',
+            'classes' => 'ml-city-country-theirs',
+            'wrapper' => false,
+        ),
+        array(
+            'title' => 'ML: Country [theirs]',
+            'inline' => 'span',
+            'classes' => 'ml-country-theirs',
+            'wrapper' => false,
+        ),
+        array(
+            'title' => 'ML: City [theirs]',
+            'inline' => 'span',
+            'classes' => 'ml-city-theirs',
+            'wrapper' => false,
+        ),
+    );
+    // Insert the array, JSON ENCODED, into 'style_formats'
+    $init_array['style_formats'] = json_encode( $style_formats );
+
+    return $init_array;
+}
+
+// Attach callback to 'tiny_mce_before_init'
+add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' );
+
+function my_theme_add_editor_styles() {
+    add_editor_style( 'custom-editor-style.css' );
+}
+
+add_action( 'admin_init', 'my_theme_add_editor_styles' );
